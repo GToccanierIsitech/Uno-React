6d93dac06d24f394f7f6535c6ad4341629cd66d2
GregoryToc <115637044+GToccanierIsitech@users.noreply.github.com>
2023-01-09 11:14:44 +0100

    Création du projet

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import './Home.scss'
+
+function Home() {
+    return (
+        <div className='HomePage'>
+            <div>
+                <h1>Se connecter</h1>
+                <input></input>
+                <input></input>
+                <p></p>
+                <button></button>
+            </div>
+        </div>
+    )
+}
+
+export default Home
